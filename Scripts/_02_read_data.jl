9582fa7bdcd82e517e1e208f262fd90711414962
Mason DeCamillis <md2477@gmail.com>
2014-04-11 21:55:43 -0400

    Added exploration of DataFrame and basic histogram using Winston

--- Scripts/_02_read_data.jl
+++ Scripts/_02_read_data.jl
@@ -3,7 +3,5 @@ include("_00_includes.jl")
 using DataFrames
 
 # Read file into DataFrame
-df = readtable("$base_folder/Data/gd80to84.txt", separator='\t')
+data = readtable("$base_folder/Data/gd80to84.txt", separator='\t')
 
-# Review the first 6 obs
-head(df)
